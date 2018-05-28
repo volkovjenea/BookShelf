b9e9c2c9a6da7c9b8c076cfeddbd8d960495bb42
Evghenii <volkov.jenea@gmail.com>
2018-05-28 22:36:16 +0300

    add validations & uniqueness

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,6 @@ class Author < ApplicationRecord
 	has_many :publications
 	has_many :books, through: :publications
 	has_many :comments, as: :commentable
+	validates :name, presence: true
+	validates :bio, presence: true
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ApplicationRecord
 	has_many :publications
 	has_many :authors, through: :publications, source: :author
 	has_many :comments, as: :commentable
+	validates :title, presence: true
+	validates :description, presence: true
+	validates :isbn, presence: true, uniqueness: true
+
 end
