ee4301c241e18b9e8de3b5138e7bf1dc91a168d9
evghenii <volkov.jenea@gmail.com>
2018-05-31 16:56:24 +0300

    style fix

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,31 @@
+#
+# frozen_string_literal: true
+
 class BooksController < ApplicationController
-  def index 
-	  @books=Book.all
+  def index
+    @books = Book.all
   end
 
   def show
     @book = Book.find(params[:id])
   end
-    
+
   def new
-    @book=Book.new
+    @book = Book.new
   end
 
   def edit
-    @book=Book.find(params[:id])  
+    @book = Book.find(params[:id])
   end
 
-	def create
-		@book=Book.new(book_params)
-		if @book.save
-		  redirect_to @book
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book
     else
       render 'new'
     end
-	end
+  end
 
   def update
     @book = Book.find(params[:id])
@@ -34,13 +37,14 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book=Book.find(params[:id])
+    @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
 
-private
- 	def book_params
-   	params.require(:book).permit(:title, :description, :isbn)
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :description, :isbn)
   end
 end
