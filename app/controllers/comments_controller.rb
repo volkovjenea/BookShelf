09f71003aefe5314c0d319d20b867680491e1921
Evghenii <volkov.jenea@gmail.com>
2018-06-14 01:11:48 +0300

    changed background add the deletion for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,11 @@ class CommentsController < ApplicationController
     @comment = @book.comments.create(params[:comment].permit(:text, :name))
     redirect_to book_path(@book)
   end
+
+  def destroy
+		@book = Book.find(params[:book_id])
+		@comment = @book.comments.find(params[:id])
+		@comment.destroy
+		redirect_to book_path(@book)
+  end
 end
